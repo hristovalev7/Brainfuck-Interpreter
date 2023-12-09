51fbe241d38117b55d899e9963d5d3b2e0d42841
Hristo Valev <hristo.valev7@gmail.com>
2023-12-09 23:38:22 +0200

    Added code that takes a file name and tries to interpret it

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
-#include <cstdio>
 #include "Interpreter.hpp"
 
 int main()
 {
+    std::string file;
+    std::cout << "File to interpret: ";
+    std::cin >> file;
+    Interpreter interpreter(file, 30000);
+    interpreter.executeCode();
     return 0;
 }
